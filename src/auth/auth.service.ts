cf83597d7ade9e9ae10121bb8d75df8b6f329d6a
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-09-12 15:31:49 -0300

    feat: create new user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { SignupDto } from 'src/dto/auth.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signin() {
-    return '';
+  async signup(dto: SignupDto) {
+    const exists = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (exists)
+      throw new HttpException('User aready exists', HttpStatus.CONFLICT);
+
+    const hash = await argon.hash(dto.password);
+
+    const user = await this.prisma.user.create({
+      data: {
+        firsName: dto.firstName,
+        lastName: dto.lastName,
+        email: dto.email,
+        passwordHash: hash,
+      },
+    });
+
+    delete user.passwordHash;
+
+    return user;
   }
 
-  signup() {
+  signin() {
     return '';
   }
 }
