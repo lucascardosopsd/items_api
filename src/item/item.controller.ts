b14c7c2f9eced311ec03430b3f859238e747edf2
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-09-13 18:07:24 -0700

    feat: get item by id

--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { CreateItemDto } from 'src/dto/item/create-item.dto';
 import { JwtGuard } from 'src/guard/jwt.guard';
 import { ItemService } from './item.service';
@@ -8,6 +8,11 @@ import { GetUser } from 'src/decorator/get-user.decorator';
 @Controller('item')
 export class ItemController {
   constructor(private readonly itemService: ItemService) {}
+
+  @Get(':id')
+  get(@GetUser('id') userId: number, @Param('id') id: number) {
+    return this.itemService.get(userId, +id);
+  }
 
   @Post()
   create(@GetUser('id') userId: number, @Body() createUserDto: CreateItemDto) {
--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -6,6 +6,12 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class ItemService {
   constructor(private readonly prisma: PrismaService) {}
 
+  get(userId: number, id: number) {
+    return this.prisma.item.findUnique({
+      where: { id, userId },
+    });
+  }
+
   create(userId: number, createItemDto: CreateItemDto) {
     return this.prisma.item.create({
       data: {
